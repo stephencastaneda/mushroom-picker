ee663a5dfbf476e7554c92d2560d7dd1d131c8bc
Stephen Castaneda <stephen.castaneda40@gmail.com>
2020-05-13 13:00:47 -0500

    mushrooms display

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.scss';
 
+import mushroomData from '../helpers/mushroomData';
+import Forest from '../components/Forest/Forest';
+
 class App extends React.Component {
+  state = {
+    mushrooms: [],
+  }
+
+  componentDidMount() {
+    const mushrooms = mushroomData.getMushrooms();
+    this.setState({ mushrooms });
+  }
+
   render() {
     return (
       <div className="App">
-        <h2>INSIDE APP COMPONENT</h2>
-        <button className="btn btn-info">CRAP</button>
+        <h1>Mushroom Picker</h1>
+        <button className="btn btn-danger p-2 mb-3">Pick A Mushroom</button>
+        <Forest mushrooms={this.state.mushrooms} />
       </div>
     );
   }
---
+++ src/components/Forest/Forest.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Forest.scss';
+
+import Mushroom from '../Mushroom/Mushroom';
+
+class Forest extends React.Component {
+  render() {
+    const { mushrooms } = this.props;
+    const makeMushrooms = mushrooms.map((mushroom) => (
+    <Mushroom key={mushroom.id} mushroom={mushroom}/>
+    ));
+
+    return (
+    <div className="forest d-flex flex-wrap">
+     {makeMushrooms}
+    </div>
+    );
+  }
+}
+export default Forest;
